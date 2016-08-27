d18000b5b5374b89f7d446bf5e7320228e2a1aa6
Shannon Moeller <me@shannonmoeller.com>
2016-08-27 09:43:12 -0400

    Updated deps and options handling.

--- src/svgstore.js
+++ src/svgstore.js
@@ -5,8 +5,8 @@ var cheerio = require('cheerio');
 
 var svgToSymbol = require('./utils/svg-to-symbol');
 
-var SELECTOR_DEFS = 'defs';
 var SELECTOR_SVG = 'svg';
+var SELECTOR_DEFS = 'defs';
 
 var TEMPLATE_SVG = '<svg xmlns="http://www.w3.org/2000/svg"><defs/></svg>';
 var TEMPLATE_DOCTYPE = '<?xml version="1.0" encoding="UTF-8"?>' +
@@ -16,7 +16,9 @@ var TEMPLATE_DOCTYPE = '<?xml version="1.0" encoding="UTF-8"?>' +
 var DEFAULT_OPTIONS = {
 	cleanDefs: false,
 	cleanObjects: false,
-	customSymbolAttrs: []
+	svgAttr: {},
+	defsAttr: {},
+	symbolAttr: {},
 };
 
 function load(text) {
@@ -44,8 +46,9 @@ function clean($, el, attrs) {
 }
 
 function svgstore(options) {
-	var parentOptions = assign({}, DEFAULT_OPTIONS, options);
+	var svgstoreOptions = assign({}, DEFAULT_OPTIONS, options);
 
+	// <svg>
 	var parent = load(TEMPLATE_SVG);
 	var parentSvg = parent(SELECTOR_SVG);
 	var parentDefs = parent(SELECTOR_DEFS);
@@ -54,37 +57,38 @@ function svgstore(options) {
 		element: parent,
 
 		add: function (id, file, options) {
-			var childOptions = assign({}, parentOptions, options);
-
 			var child = load(file);
-			var childDefs = child(SELECTOR_DEFS);
+			var addOptions = assign({}, svgstoreOptions, options);
 
-			var cleanDefs = childOptions.cleanDefs;
+			// <defs>
+			var childDefs = child(SELECTOR_DEFS);
+			var cleanDefs = addOptions.cleanDefs;
+
 			if (cleanDefs) {
 				clean(child, childDefs, cleanDefs);
 			}
+
 			parentDefs.append(childDefs.contents());
 			childDefs.remove();
 
-			var childSymbol = svgToSymbol(id, child, childOptions);
+			// <symbol>
+			var childSymbol = svgToSymbol(id, child, addOptions);
+			var cleanObjects = addOptions.cleanObjects;
 
-			// clean <symbol/>
-			var cleanObjects = childOptions.cleanObjects;
 			if (cleanObjects) {
 				clean(child, childSymbol, cleanObjects);
 			}
 
-			// append <symbol/>
 			parentSvg.append(childSymbol);
 
 			return this;
 		},
 
 		toString: function (options) {
+			var toStringOptions = assign({}, svgstoreOptions, options);
 			var xml = parent.xml();
-			var inline = options && options.inline;
 
-			if (!inline) {
+			if (!toStringOptions.inline) {
 				return TEMPLATE_DOCTYPE + xml;
 			}
 
